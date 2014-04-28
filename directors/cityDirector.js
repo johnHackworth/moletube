3df18a40a164ac10a01d9d22660cd7ac2a6125b1
Javier Alvarez <javier.alvarez@taptapnetworks.com>
2014-04-28 16:12:09 +0200

    fix lag

--- directors/cityDirector.js
+++ directors/cityDirector.js
@@ -64,7 +64,7 @@ window.moletube.currentStage = new pixEngine.Stage({
     this.cityVisible = true;
     this.city = new moletube.models.City({
       x: 600,
-      y: -85,
+      y: -50,
       stage: this
     })
     this.engine.addEntity(this.city);
