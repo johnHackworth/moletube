21515846bd4a8ef20617cb3d67c08f3a58293387
Javier Alvarez <javier.alvarez@taptapnetworks.com>
2014-05-12 14:54:10 +0200

    work

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 window.moletube = window.moletube || {};
 window.moletube.config = window.moletube.config || {};
-window.moletube.config.width = 1200;
-window.moletube.config.height = 800;
+window.moletube.config.width = window.innerWidth;// 1200;
+window.moletube.config.height = window.innerHeight; //800;
 window.moletube.config.tileWidth = 50;
