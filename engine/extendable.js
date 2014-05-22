bc48ab632db53f2ea39fc70ff8a129839ff49a1b
johnHackworth <javieralvarezlop@gmail.com>
2014-05-22 09:35:33 +0200

    profile stuff

--- engine/extendable.js
+++ engine/extendable.js
@@ -1,8 +1,8 @@
 window.pixEngine = window.pixEngine || {};
 window.pixEngine.utils = window.pixEngine.utils || {};
 
-window.pixEngine.utils.extend = function(object, keepExisting) {
-  object.call(this);
+window.pixEngine.utils.extend = function(object, keepExisting, options) {
+
   for (var n in object.prototype) {
     if (!keepExisting) {
       this[n] = object.prototype[n];
@@ -10,4 +10,5 @@ window.pixEngine.utils.extend = function(object, keepExisting) {
       this[n] = object.prototype[n];
     }
   }
+  object.call(this, options);
 };
